0bb5b561b796627df436a931d82dc22692923596
Olesyakorzh <107040813+Olesyakorzh@users.noreply.github.com>
2023-11-17 15:15:48 +0300

    Create lecture7.py
    статические методы

---
+++ lecture7.py
@@ -0,0 +1,36 @@
+class Animal:
+    
+    def voice(self):
+        pass
+    num_of_insts = 0
+    def __init__(self):
+        Animal.num_of_insts = Animal.num_of_insts + 1
+    
+class Volf(Animal):
+     def voice(self):
+         print('Волк воет')
+         
+
+volf = Volf()
+volf.voice()
+
+class Fish(Animal):
+    def voice(self):
+        print('Рыба молчит')
+        
+fish = Fish()
+fish.voice()
+
+class Bird(Animal):
+    def voice(self):
+        print('Птичка поет')
+        
+bird = Bird()
+bird.voice()
+
+def print_num():
+    print(Animal.num_of_insts)
+    
+print_num=staticmethod(print_num)
+
+print_num()
