4573f759536d738d76a3087c574c93ac0f000d39
Olesyakorzh <107040813+Olesyakorzh@users.noreply.github.com>
2023-11-27 10:15:40 +0300

    Update lecture9.py
    правки

--- lecture9.py
+++ lecture9.py
@@ -1,28 +1,35 @@
 from threading import Thread
 from time import sleep
 
-def Flow1():
+def Flow(thread_name):
+    ###todo. Сообщение, что старт потока 1.
     for i in range(10, 0, -1):
         print(f'Из первого потока: {i}')
         sleep(1)
-        
-def Flow2():
+        ###todo. Сообщение конца потока 1.
+       
     for i in range(10, 0, -1):
         print(f'Из второго потока: {i}')
-        sleep(1)
-        
-flow1= Thread(target=Flow1)
-print(f"thread status: {flow1.is_alive()}")
+        sleep(1)  
+         ###todo. Сообщение конца потока 2.
+### Формирование задач для запуска в параллель.
+
+flow1= Thread(target=Flow, args=(1, ))                    
+flow2= Thread(target=Flow, args=(2,))
+
+###Запуск потоков.
 flow1.start()
+sleep(2.1)
 print(f"thread status: {flow1.is_alive()}")
-flow1.join()
-flow2= Thread(target=Flow2)
-print(f"thread status: {flow2.is_alive()}")
 flow2.start()
 print(f"thread status: {flow2.is_alive()}")
+###Объединение потоков
+flow1.join()
 flow2.join()
-sleep(2)
+print(f"thread status: {flow1.is_alive()}")
 print(f"thread status: {flow2.is_alive()}")
+
+###todo Сообщение, что все потоки завершились.
 
        
 
